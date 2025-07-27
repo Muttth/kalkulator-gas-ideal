6e5b2976e8345901ab707ca61008e9eebcacfe22
Muttth <mutianingrmvr@gmail.com>
2025-07-27 22:14:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://media.giphy.com/media/l3vR85PnGsBwu1PFK/giphy.gif");
+        background-image: url("https://www.canva.com/templates/EAGUJkNjyJY/");
         background-size: cover;
         background-attachment: fixed;
     }
